9b91df734dda91d8c1e7d9b12ede727475eeba5f
Vladimir Migashko <migashko@gmail.com>
2013-11-22 18:32:09 +0400

    upd

--- core/build_info.h
+++ core/build_info.h
@@ -3,8 +3,8 @@ namespace
   const char* build_info=
     "\n"
     "Branch: devel\n"
-    "commit b00616a5c4b8940c23d871f9b077a445bc5f19d1\n"
+    "commit 795d1f3caf62a3a4ff1ee0cde4b59f43b8f9e9f2\n"
     "Author: Vladimir Migashko <migashko@gmail.com>\n"
-    "Date: Fri Nov 22 01:50:45 2013 +0400\n"
+    "Date: Fri Nov 22 18:15:53 2013 +0400\n"
     "upd\n";
 }
