84f85e101a4962b608070e569e4181e9b79a40d4
Vladimir Migashko <migashko@gmail.com>
2013-12-05 03:09:36 +0400

    eupd

--- core/core_config_json.hpp
+++ core/core_config_json.hpp
@@ -14,7 +14,7 @@ struct core_config_json
   
   typedef json::object<
     core_config,
-    type_list_n<
+    fas::type_list_n<
        json::member<n_enabled, core_config, bool, &core_config::enabled>,
        json::member<n_idle_timeout_ms, core_config, time_t, &core_config::idle_timeout_ms>,
        json::member<n_wait_timeout_ms, core_config, time_t, &core_config::wait_timeout_ms>
