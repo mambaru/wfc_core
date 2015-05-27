e351a736c8ee1ae41aa7116ee8f66d96dc0e96a3
Vladimir Migashko <migashko@gmail.com>
2015-05-27 20:47:24 +0300

    upd

--- logger/logger_package.cpp
+++ logger/logger_package.cpp
@@ -5,6 +5,7 @@
 //
 
 
+#include "logger_build_info.h"
 #include "logger_package.hpp"
 #include "logger_module.hpp"
 #include <wfc/module/module_list.hpp>
@@ -15,7 +16,7 @@ namespace wfc{
 WFC_NAME2(logger_package_name, "logger")
 
 class logger_package_impl: public ::wfc::module_list<
-  logger_package_name,
+  logger_build_info,
   logger_module
 >
 {  
