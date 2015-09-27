1a934a0007bc51eed54373ffc0ed292dd31ff7e8
Vladimir Migashko <migashko@gmail.com>
2015-09-28 01:17:15 +0300

    upd

--- core/core_config.hpp
+++ core/core_config.hpp
@@ -8,6 +8,7 @@ struct core_config
 {
   time_t idle_timeout_ms = 1000;
   size_t rlimit_as_mb = 0;
+  bool enable_callback_check = true;
 };
 
 }
--- core/core_config_json.hpp
+++ core/core_config_json.hpp
@@ -9,12 +9,14 @@ struct core_config_json
 {
   JSON_NAME(idle_timeout_ms)
   JSON_NAME(rlimit_as_mb)
+  JSON_NAME(enable_callback_check)
   
   typedef json::object<
     core_config,
     fas::type_list_n<
        json::member<n_idle_timeout_ms, core_config, time_t, &core_config::idle_timeout_ms>,
-       json::member<n_rlimit_as_mb, core_config, size_t, &core_config::rlimit_as_mb >
+       json::member<n_rlimit_as_mb, core_config, size_t, &core_config::rlimit_as_mb >,
+       json::member<n_enable_callback_check, core_config, bool, &core_config::enable_callback_check >
     >::type
   > type;
   
