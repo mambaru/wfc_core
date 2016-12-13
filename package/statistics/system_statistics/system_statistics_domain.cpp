384d443c1a7b7e67f58c56440862349861aa3ccf
Vladimir Migashko <migashko@gmail.com>
2016-12-13 19:43:44 +0300

    *system stat

--- package/statistics/system_statistics/system_statistics_domain.cpp
+++ package/statistics/system_statistics/system_statistics_domain.cpp
@@ -51,8 +51,8 @@ void system_statistics_domain::initialize()
         this->create_meter(proto->stime,  0, ps.stime - proto->ps.stime );
         this->create_meter(proto->cutime, 0, ps.cutime - proto->ps.cutime );
         this->create_meter(proto->cstime, 0, ps.cstime - proto->ps.cstime );
-        this->create_meter(proto->vsize,  ps.vsize, 0);
-        this->create_meter(proto->rss,  ps.rss, 0);
+        this->create_meter(proto->vsize,  ps.vsize/(1024*1024), 0);
+        this->create_meter(proto->rss,  ps.rss*getpagesize()/(1024*1024), 0);
         proto->ps = ps;
       }
       return true;
