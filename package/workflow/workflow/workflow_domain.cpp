766eb44bd16a12848de08883fa547b75f4d2a052
Vladimir Migashko <migashko@gmail.com>
2016-12-20 18:22:08 +0300

    upd

--- package/workflow/workflow/workflow_domain.cpp
+++ package/workflow/workflow/workflow_domain.cpp
@@ -18,18 +18,23 @@ workflow_domain::~workflow_domain()
 {
 }
 
+void workflow_domain::configure() 
+{
+  this->reconfigure();
+}
+
 void workflow_domain::reconfigure() 
 {
+  auto name = this->name();;
   auto opt = this->options();
-  opt.id = this->name();
+  opt.id = name;
   if ( _workflow == nullptr )
   {
     _workflow = std::make_shared<impl>( this->global()->io_service, opt );
     if ( auto g = this->global() )
-      g->registry.set( "workflow", this->name(), _workflow );
+      g->registry.set( "workflow", name, _workflow );
   }
-  
-  _workflow->reconfigure( opt );
+
 }
 
 void workflow_domain::initialize()
@@ -53,12 +58,12 @@ void workflow_domain::initialize()
         {
           std::stringstream ss;
           ss << this->name() << opt.stat.thread << i;
-          _meters_threads.push_back( stat->create_value_prototype(ss.str()) );
+          //_meters_threads.push_back( stat->create_value_prototype(ss.str()) );
           _counters.push_back(0);
         }
       }
 
-      if ( _meter_size == nullptr && _meter_drop==nullptr && _meters_threads.empty() )
+      if ( _meter_size == nullptr && _meter_drop==nullptr /*&& _meters_threads.empty()*/ )
         return;
 
       _stat_timer = core->create_timer( std::chrono::milliseconds(opt.stat.interval_ms), [this, stat]()->bool
@@ -72,9 +77,9 @@ void workflow_domain::initialize()
           size_t threads = this->_workflow->manager()->get_threads();
           for ( size_t i =0 ; i < threads; ++i )
           {
-            size_t counter = this->_workflow->manager()->get_counter(i) - this->_counters[i];
+            //size_t counter = this->_workflow->manager()->get_counter(i) - this->_counters[i];
             this->_counters[i] = this->_workflow->manager()->get_counter(i);
-            stat->create_meter( _meters_threads[i], 0, counter );
+            /*stat->create_meter( _meters_threads[i], 0, counter );*/
           }
         }
         
@@ -87,7 +92,50 @@ void workflow_domain::initialize()
 
 void workflow_domain::start()
 {
+  this->ready();
   _workflow->start();
+}
+
+void workflow_domain::ready() 
+{
+  auto opt = this->options();
+  auto g = this->global();
+  auto name = this->name();
+  opt.startup_handler = [g, name](std::thread::id)
+  {
+    g->threads.reg_thread(name);
+  };
+  opt.finish_handler = [g](std::thread::id)
+  {
+    g->threads.unreg_thread();
+  }; 
+  if ( this->get_statistics() != nullptr )
+  {
+    std::weak_ptr<workflow_domain> wthis = this->shared_from_this();
+    value_meter_ptr proto;
+    auto tcount = std::make_shared< std::atomic<int> >();
+    opt.statistics_handler  = [wthis, proto, tcount, opt](std::thread::id, size_t count, workflow_options::statistics_duration span) mutable
+    {
+      if ( auto pthis = wthis.lock() )
+      {
+        if ( auto stat = pthis->get_statistics() )
+        {
+          if ( proto == nullptr )
+          {
+          
+            size_t count = tcount->fetch_add(1);
+            std::stringstream ss;
+            ss << pthis->name() << opt.stat.thread << count;
+            proto = stat->create_value_prototype( ss.str());
+          }
+          
+          stat->create_meter(proto, count, std::chrono::duration_cast<std::chrono::microseconds>(span).count() );
+        }
+      }
+    };
+  }
+  _workflow->reconfigure( opt );
+
 }
 
 void workflow_domain::stop() 
--- package/workflow/workflow/workflow_domain.hpp
+++ package/workflow/workflow/workflow_domain.hpp
@@ -11,23 +11,27 @@ namespace wfc{ namespace core{
 
 class workflow_domain
   : public domain_object<iinterface, workflow_config>
+  , public std::enable_shared_from_this<workflow_domain>
 {
   class impl;
 public:
   typedef std::shared_ptr< ::wfc::value_meter > value_meter_ptr;
+  typedef std::shared_ptr< ::wfc::time_meter > time_meter_ptr;
   typedef ::wfc::workflow domain_interface;
   virtual ~workflow_domain();
   using domain_object<iinterface, workflow_config>::create;
+  virtual void configure() override;
   virtual void reconfigure() override;
   virtual void initialize() override;
   virtual void start() override;
+  virtual void ready() override;
   virtual void stop() override;
 private:
   std::shared_ptr<impl> _workflow;
   timer_id_t _stat_timer;
   value_meter_ptr _meter_size;
   value_meter_ptr _meter_drop;
-  std::vector<value_meter_ptr> _meters_threads;
+  std::map< std::thread::id, time_meter_ptr> _meters_threads;
   
   size_t _dropped = 0;
   std::vector<size_t> _counters;
