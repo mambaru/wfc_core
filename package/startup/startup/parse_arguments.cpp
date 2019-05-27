aa2f5e09cc4f5901ea77de4d0a638ea7e5172ae5
Vladimir Migashko <migashko@gmail.com>
2019-05-27 23:29:42 +0300

    clang def fix

--- package/startup/startup/parse_arguments.cpp
+++ package/startup/startup/parse_arguments.cpp
@@ -19,13 +19,12 @@ namespace
 void parse_arguments(program_arguments& pa, int argc, char* argv[])
 try
 {
-#define GCC_COMPILER (defined(__GNUC__) && !defined(__clang__))
-#if GCC_COMPILER
+#if defined(__GNUC__) && !defined(__clang__)
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
 #endif
   pa.program_name = ::boost::filesystem::path(argv[0]).filename().native();
-#if GCC_COMPILER
+#if defined(__GNUC__) && !defined(__clang__)
   #pragma GCC diagnostic pop
 #endif
   
